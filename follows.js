852dfe067214ce2c1528752138422ae9791d72c8
RisingSummer <RisingSummer578@gmail.com>
2014-05-01 16:22:24 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,8 +17,8 @@
 		applicationName	= 'RisingSummer', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor =	'0A1922', // CSS HEX value of color to represent data (omit leading #)
+		hideForm	1	= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
